732a41b2aa7f6c5e7a61df630cd582232f522eee
Shreya Lambodari <shreyalambodari284@gmail.com>
2026-07-29 14:50:48 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Deque<Integer> s = new ArrayDeque<>();
+
+        int ans[] = new int[temperatures.length];
+
+        for (int i = temperatures.length - 1; i >= 0; i--) {
+            
+            while (!s.isEmpty() && temperatures[s.peek()] <= temperatures[i]) {
+                s.pop();
+            }if (s.isEmpty()) {
+                ans[i] = 0;
+            }else{
+                ans[i] = s.peek()-i;
+            }
+            s.push(i);
+        }
+        return ans;
+    }
+}
